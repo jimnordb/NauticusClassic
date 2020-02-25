6a5fe83a11c3eed31e6a412b9128b4095a20fda8
DungFu <DungFu@users.noreply.github.com>
2020-02-24 23:35:59 -0800

    Revert "Update to spam channels a little bit less"
    This reverts commit 03b4d84354896e7726eade44a3f379a1d554209e.

--- NautComms.lua
+++ NautComms.lua
@@ -24,19 +24,19 @@ function NauticusClassic:CancelRequest()
 	end
 end
 
-function NauticusClassic:DoRequest(wait, whisper_target)
+function NauticusClassic:DoRequest(wait)
 	self:CancelRequest()
 
 	if wait then
-		request = self:ScheduleTimer("DoRequest", wait, whisper_target)
+		request = self:ScheduleTimer("DoRequest", wait)
 		return
 	end
 
 	if next(requestList) then
-		self:BroadcastTransportData(whisper_target)
+		self:BroadcastTransportData()
 
 		if requestVersion or next(requestList) then
-			self:DoRequest(2.5, whisper_target)
+			self:DoRequest(2.5)
 			return
 		end
 	end
@@ -45,7 +45,7 @@ function NauticusClassic:DoRequest(wait, whisper_target)
 		requestVersion = false
 		local version = self.versionNum
 		if self.version then version = version.." "..self.version; end
-		self:SendMessage(CMD_VERSION.." "..version, whisper_target)
+		self:SendMessage(CMD_VERSION.." "..version)
 	end
 end
 
@@ -119,7 +119,7 @@ local function StringHash(text)
 	return counter % 4294967291
 end
 
-function NauticusClassic:BroadcastTransportData(whisper_target)
+function NauticusClassic:BroadcastTransportData()
 	local since, boots, swaps
 	local lag = GetLag()
 	local trans_str = ""
@@ -151,7 +151,7 @@ function NauticusClassic:BroadcastTransportData(whisper_target)
 
 	if trans_str ~= "" then
 		trans_str = strsub(trans_str, 1, -2) -- remove the last comma
-		self:SendMessage(CMD_KNOWN.." "..crunch(DATA_VERSION).." "..trans_str.." "..crunch(StringHash(trans_str)), whisper_target)
+		self:SendMessage(CMD_KNOWN.." "..crunch(DATA_VERSION).." "..trans_str.." "..crunch(StringHash(trans_str)))
 		self:DebugMessage("tell our transports ; length: "..strlen(trans_str))
 	else
 		self:DebugMessage("nothing to tell")
@@ -174,21 +174,13 @@ function NauticusClassic:RequestTransport(t)
 	requestList[t] = true
 end
 
-function NauticusClassic:SendMessage(msg, whisper_target)
+function NauticusClassic:SendMessage(msg)
 	if not self.comm_disable then
-		if whisper_target then
-			self:DebugMessage("sending: "..msg.." to: "..whisper_target)
-			self:SendCommMessage(self.DEFAULT_PREFIX, msg, "WHISPER", whisper_target)
-		else
-			self:DebugMessage("sending: "..msg.." to all")
-			if IsInGroup() then
-				self:SendCommMessage(self.DEFAULT_PREFIX, msg, "RAID")
-			end
-			if IsInGuild() then
-				self:SendCommMessage(self.DEFAULT_PREFIX, msg, "GUILD")
-			end
-			self:SendCommMessage(self.DEFAULT_PREFIX, msg, "YELL")
+		self:SendCommMessage(self.DEFAULT_PREFIX, msg, "RAID")
+		if IsInGuild() then
+			self:SendCommMessage(self.DEFAULT_PREFIX, msg, "GUILD")
 		end
+		self:SendCommMessage(self.DEFAULT_PREFIX, msg, "YELL")
 	end
 end
 
@@ -230,7 +222,7 @@ function NauticusClassic:ReceiveMessage_version(clientversion, sender)
 		end
 	elseif clientversion < self.versionNum then
 		requestVersion = true
-		self:DoRequest(5 + math.random() * 15, sender)
+		self:DoRequest(5 + math.random() * 15)
 		return
 	end
 
@@ -304,7 +296,7 @@ function NauticusClassic:ReceiveMessage_known(version, transports, hash, sender)
 
 	-- if we don't need to send back any data, cancel our scheduler immediately
 	if next(requestList) then
-		self:DoRequest(5 + math.random() * 15, sender)
+		self:DoRequest(5 + math.random() * 15)
 	elseif not requestVersion then
 		self:DebugMessage("received: known; no more to send")
 		self:CancelRequest()
